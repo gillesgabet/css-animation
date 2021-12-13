74cfed8b13a84207126155d2bd847c2ebfda5088
gig <gil.gabet@gmail.com>
2021-12-13 11:15:06 +0100

    collapsemenu

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,17 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { CubeComponent } from './components/cube/cube.component';
+import { IndexComponent } from './components/index/index.component';
 import { BubbleMenuComponent } from './components/menu/bubble-menu/bubble-menu.component';
 
 const routes: Routes = [
-  { path: 'css/menu/bubble'       , component: BubbleMenuComponent},
-  { path: 'css/cube'              , component: CubeComponent },
-  { path: '**'              , component: AppComponent },
+  { path: 'css', component: IndexComponent, children: [
+    { path: 'menu/bubble'       , component: BubbleMenuComponent, outlet: 'menu'},
+    { path: 'cube'              , component: CubeComponent, outlet: 'content' },
+  ]},
+  
+  { path: 'index'                 , component: IndexComponent },
+  { path: '**'                    , redirectTo: 'index', pathMatch: 'full' },
 
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,12 +11,14 @@ import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule} from '@angular/material/button';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { IndexComponent } from './components/index/index.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CubeComponent,
-    BubbleMenuComponent
+    BubbleMenuComponent,
+    IndexComponent
   ],
   imports: [
     BrowserModule,
